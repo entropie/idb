750c7322cb585a75e90f56232e2f6e6d70a38fbe
Michael Trommer <mictro@gmail.com>
2011-02-25 16:11:32 +0100

    fancybox for slided images

--- app/public/js/idb.js
+++ app/public/js/idb.js
@@ -12,23 +12,40 @@
 
 })(jQuery);
 
+$.tools.tabs.addEffect("myslide", function(i, done) {
+  this.getPanes().hide();
+
+  this.getPanes().eq(i).slideDown("slow", function()  {
+    $(".img-box img", this).each(fancyBoxAni);
+    done.call();
+  });
+});
+
+function fancyBoxAni(){
+  var img = $(this);
+  $(img).fancybox({
+    href: $(this).attr('data-org'),
+    transitionIn: 'elastic',
+    transitionOut: 'elastic'
+  });
+}
+
+
+
 $(document).ready(function () {
   $("#preload a").preload({
     threshold: 3
     //onFinish: function(){ alert("preloaded");}
-});
-
+  });
 
   $(".img-box").setupImage();
   $("#thumb-tabs").tabs("#thumb-content > div", {
-    effect: 'fade',
+    effect: 'myslide',
     fadeInSpeed: 1000,
     fadeOutSpeed: 0,
     rotate: true,
     onBeforeClick: function(event, i) {
-
       var pane = this.getPanes().eq(i);
-
       if (pane.is(":empty")) {
         pane.load(this.getTabs().eq(i).attr("href"));
       }
@@ -36,12 +53,5 @@ $(document).ready(function () {
   }).slideshow({clickable: false});
   //$("ul#thumb-tabs").data("slideshow").play();
 
-  $(".img-box img").each(function(){
-    var img = $(this);
-    $(img).fancybox({
-      href: $(this).attr('data-org'),
-      transitionIn: 'elastic',
-      transitionOut: 'elastic'
-    });
-  });
+  $(".img-box img").each(fancyBoxAni);
 });
